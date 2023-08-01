bcdf81a33ca3da3fe30ebaa61feb86c0cdcb2647
Gonçalo Nogueira <gn16102000@gmail.com>
2023-08-01 19:21:28 +0100

    navbar quase

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,5 @@
 import {
-    Flex,
+    Flex, Image, Link
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -7,8 +7,10 @@ import React from "react";
 export default function PortfolioWindow() {
 
     return (
-        <Flex w="100vw" h="50px" bg="white" position="fixed" zIndex={200}>
+        <Flex w="100vw" h="100px" bg="linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0))" position="fixed" zIndex={200} alignItems="start" justifyContent="space-between" px={10} py={7}>
+            <Link href="/"><Image src="\pgp-logo.png" h="60px" mt={-4}></Image></Link>
 
+            <Image src="\hamburger.png" h="15px" mr="20px"></Image>
         </Flex >
     );
 }
